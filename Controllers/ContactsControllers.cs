3d5a3a600b7ce446cfc34741812192a0ae5e7b5b
v-bndiwa <v-bndiwa@DESKTOP-IMM1IDF>
2023-08-28 13:10:05 +0300

    Get All Contacts

---
+++ Controllers/ContactsControllers.cs
@@ -0,0 +1,24 @@
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    //[Route("/api/[controller]")]
+    [Route("/api/contacts")]
+    public class ContactsControllers : Controller
+    {
+        private readonly ContactsAPIDbContext contactsAPIDbContext;
+
+        public ContactsControllers(ContactsAPIDbContext contactsAPIDbContext)
+        {
+            this.contactsAPIDbContext = contactsAPIDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            return Ok(contactsAPIDbContext.Contacts.ToList());
+        }
+    }
+}
